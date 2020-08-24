66453f0c2b2a7862091f70c613eac3af07941beb
Ansar Bahrun <ansarbahrun@Ansars-MacBook-Air.local>
2020-08-23 17:44:26 -0700

    Adding all the numbers in an array. Printing the sum.

---
+++ script.js
@@ -0,0 +1,11 @@
+// Declare an array
+var myArr = [ 2, 3, 4, 5, 6];
+// initialize variable to 0
+var total = 0;
+//iterate through the loop
+for (var i = 0; i < myArr.length; i++) {
+  //add the numbers in the array to total 
+   total = total + myArr[i];
+}
+
+console.log(total)
